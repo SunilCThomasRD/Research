d3a3f7029b5b86cd9b350e50e0f20ee715c1751d
SunilCThomasRD <sunil.thomas@nashtechlabs.com>
2023-01-25 11:54:14 +0530

    Description of main function added

--- Dummy.c
+++ Dummy.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ *	@Description: Main Function of this project.
+ *	@Function_Name: main
+ *	@Parameters: None.
+ *	@Return: int - 0-> True, 1-> False
+*/
 int main(void)
 {
 	printf("Hi Sunil\r\n");
