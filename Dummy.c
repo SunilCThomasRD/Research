4c49e9ed1d38a632cdd455e9fe587f6d114134fa
SunilCThomasRD <sunil.thomas@nashtechlabs.com>
2023-01-27 15:23:50 +0530

    Application started

--- Dummy.c
+++ Dummy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/**************Functin Prototypes*****************/
+int School(void);
 
-/************************************************
+/*************************************************
  *	@Description: Main Function of this project.
  *	@Function_Name: main
  *	@Parameters: None.
@@ -9,5 +11,18 @@
 int main(void)
 {
 	printf("Hi Sunil\r\n");
+	School();
+	return 0;
+}
+
+/*************************************************
+ *	@Description: Function for school.
+ *	@Function_Name: School
+ *	@Parameters: None.
+ *	@Return: int - 0-> True, 1-> False
+**************************************************/
+int School(void)
+{
+	printf("This is School Function\r\n");
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Research
 Playground for understanding GIT
+
+25/01/2023 ---- Initial Commit. Basic structure completed.
+27/01/2023 ---- Application started.
