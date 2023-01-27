cc9a225cf5f1bedb1ab1d5c90b3aa98171c6f813
SunilCThomasRD <sunil.thomas@nashtechlabs.com>
2023-01-27 15:58:22 +0530

    Application edited from user 1

--- Dummy.c
+++ Dummy.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**************Functin Prototypes*****************/
 int School(void);
+int Display(int u32Display_Mode);
 
 /*************************************************
  *	@Description: Main Function of this project.
@@ -11,6 +12,7 @@ int School(void);
 int main(void)
 {
 	printf("Hi Sunil\r\n");
+	School();
 	School();
 	return 0;
 }
@@ -24,5 +26,24 @@ int main(void)
 int School(void)
 {
 	printf("This is School Function\r\n");
+	return 0;
+}
+
+/*************************************************
+ *	@Description: Function for display.
+ *	@Function_Name: Display
+ *	@Parameters: int u32Display_Mode - 1 - Big Display, 0 - Small Display.
+ *	@Return: int - 0-> True, 1-> False
+**************************************************/
+int Display(int u32Display_Mode)
+{
+	if(u32Display_Mode)
+	{
+		printf("This is Big Display\r\n");
+	}
+	else
+	{
+		printf("This is Small Display\r\n");
+	}
 	return 0;
 }
