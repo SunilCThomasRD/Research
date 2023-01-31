9779316ea0c1c5ea806bdad106b66b7fbe531e99
SunilCThomasRD <sunil.thomas@nashtechlabs.com>
2023-01-31 09:48:49 +0530

    Initial commit from branch_r_d

--- Dummy.c
+++ Dummy.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+/**************Functin Prototypes*****************/
 
+/*************************************************
+ *	@Description: Main Function of this project.
+ *	@Function_Name: main
+ *	@Parameters: None.
+ *	@Return: int - 0-> True, 1-> False
+**************************************************/
+int main(void)
+{
+	printf("This is from branch_r_d\n");
+	return 0;
+}
